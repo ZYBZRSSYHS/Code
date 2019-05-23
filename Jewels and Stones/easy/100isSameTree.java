d17df0a3ce6d86c2406241d207e1f18e4de8059a
ZYBZRSSYHS <43528911+ZYBZRSSYHS@users.noreply.github.com>
2019-05-23 10:25:37 +0800

    Update 100isSameTree.java

--- Jewels and Stones/easy/100isSameTree.java
+++ Jewels and Stones/easy/100isSameTree.java
@@ -10,7 +10,7 @@
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p==null && q==null)return true;
-        if(p==null||p==null)return false;
+        if(q==null||p==null)return false;
         if(p.val !=q.val)return false;
         return isSameTree(p.right,q.right)&&
         isSameTree(p.left,q.left);
