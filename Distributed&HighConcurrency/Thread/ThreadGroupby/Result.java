2474cae1ebf4592a117be196c0a0004fb4cbb527
ZYBZRSSYHS <1422246929@qq.com>
2019-05-30 23:45:57 +0800

    提交信息

---
+++ Distributed&HighConcurrency/Thread/ThreadGroupby/Result.java
@@ -0,0 +1,13 @@
+public class Result{
+    private String name;
+    
+    public char getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public static void main(){
+
+    }
+}
