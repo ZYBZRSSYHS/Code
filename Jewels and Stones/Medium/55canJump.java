b9fadaeb837cff611c3cd9612c32d929ee5a9abc
ZYBZRSSYHS <1422246929@qq.com>
2019-09-28 11:20:51 +0800

    enter

--- Jewels and Stones/Medium/55canJump.java
+++ Jewels and Stones/Medium/55canJump.java
@@ -1,45 +1,11 @@
-enum Index{
-    GOOD,BAD,UNKNOWN
-}
 class Solution {
-    Index[] memo;
-    public boolean canJump(int[] nums){
-        memo = new Index[nums.length];
-
-        for(int i=0;i<memo.length;i++){
-            memo[i]=Index.UNKNOWN;
-        }
-        
-        memo[memo.length-1] = Index.GOOD;
-        return canJumpFromPosition(0, nums);
-    }
-    public boolean canJumpFromPosition(int position,int[] nums){
-        if(memo[position] !=Index.UNKNOWN){
-            return memo[position] == Index.GOOD?true:false;
-        }
-        int furthestJump = Math.min(position+nums[position], nums.length-1);
-        for(int nextPosition = position+1;nextPosition<=furthestJump;nextPosition++){
-            if(canJumpFromPosition(nextPosition, nums)){
-                memo[position] = Index.GOOD;
-                return true;
+    public boolean canJump(int[] nums) {
+        int leftIndex = nums.length-1;
+        for(int i=nums.length-2;i>=0;i--){
+            if(nums[i]+i>=leftIndex){
+                leftIndex=i;
             }
         }
-        memo[position] = Index.BAD;
-        return false;
+        return leftIndex==0;
     }
-    // public boolean canJump(int[] nums) {
-    //     return canJumpFromPosition(0, nums);
-    // }
-    // public boolean canJumpFromPosition(int position,int[] nums){
-    //     if(position == nums.length-1){
-    //         return true;
-    //     }
-    //     int furthestJump = Math.min(position+nums[position], nums.length-1);
-    //     for(int nextPosition = position+1;nextPosition<=furthestJump;nextPosition++){
-    //         if(canJumpFromPosition(nextPosition, nums)){
-    //             return true;
-    //         }
-    //     }
-    //     return false;
-    // }
 }
